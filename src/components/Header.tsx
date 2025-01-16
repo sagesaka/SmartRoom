ab93c0822c5595c14c7581b090772b1cf364e16b
sagesaka <sagesaka@riprice.co.jp>
2025-01-16 13:46:38 +0900

    ログイン/ログアウト機能を実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
-import EventAvailable from '@mui/icons-material/EventAvailable'
+import { useState, useEffect } from 'react';
+import EventAvailable from '@mui/icons-material/EventAvailable';
+import Person from '@mui/icons-material/Person';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { signInWithGoogle, auth } from '../Firebase';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 
 const headerBackground = css`
   display: flex;
@@ -49,17 +53,89 @@ const login = css`
     right: 0;
   }
 `
+const userIcon = css`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  position: relative;
+  cursor: pointer;
+`
+const logout = (isClick: boolean) => css`
+  position: absolute;
+  top: 50px;
+  right: 0;
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  padding: 10px;
+  z-index: 2;
+  min-width: 100px;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  opacity: ${isClick ? 1 : 0};
+  transform: scale(${isClick? 1 : 0.95});
+`
+const StyledHR = styled.hr`
+  border: none;
+  border-bottom: 1px solid #ccc;
+`
 
 const Header = () => {
+  const [user, setUser] = useState(null);
+  const [logoutWindow, setLogoutWindow] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // ログイン機能
+  const handleLogin = async () => {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      alert("ログインに失敗しました")
+    }
+  }
+  
+  // ログアウト機能
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setLogoutWindow(false);
+      alert("ログアウトしました");
+    } catch (error) {
+      console.error("ログアウトに失敗しました", error)
+    }
+  };
+  
   return (
     <div css={headerBackground}>
       <div css={appTitle}>
         <EventAvailable css={iconStyle} />
         SmartRoom
       </div>
-      <div css={login}>
-        Login
-      </div>
+      {user ? (
+        <div css={userIcon} onClick={() => setLogoutWindow(!logoutWindow)}>
+          <img src={user.photoURL || <Person />}
+          css={userIcon}
+          title={user.displayName || 'User'}
+          />
+          <div css={logout(logoutWindow)} onClick={handleLogout}>
+            <div>{user.displayName } {user.email}</div>
+            <StyledHR />
+            <div onClick={handleLogout}>ログアウト</div>
+          </div>
+        </div>
+      ) : (
+        <div css={login} onClick={handleLogin}>
+          ログイン
+        </div>
+      )}
     </div>
   )
 }
